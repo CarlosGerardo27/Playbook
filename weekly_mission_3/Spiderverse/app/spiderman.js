c110efeb8f87d46f3746f5357d2575be6ee5902c
Carlos Gerardo <carlosgerardo.rocha.s@gmail.com>
2022-04-19 19:42:01 -0500

    unite test for getinfo method was added

--- weekly_mission_3/Spiderverse/app/spiderman.js
+++ weekly_mission_3/Spiderverse/app/spiderman.js
@@ -1,10 +1,13 @@
 class Spiderman{
     constructor (name, age, actor, movies, studio ){
-/*         this.name = name;
-        this.age = age;
-        this.actor = actor;
-        this.movies = movies;
-        this.studio = studio; */
+        this._name = name;
+        this._age = age;
+        this._actor = actor;
+        this._movies = movies;
+        this._studio = studio;        
+    }
+    getInfo(){
+        return `Hey! im ${this._actor} from ${this._studio}`
     }
 }
 
--- weekly_mission_3/Spiderverse/test/spiderman.test.js
+++ weekly_mission_3/Spiderverse/test/spiderman.test.js
@@ -5,12 +5,21 @@ describe("Test Suite for spiderman Class", () => {
     test('Create of a spiderman object', () => {
         const andrewGarfield = new Spiderman("Spiderman Sony", 31 , "Andrew Garfield", 2 , "Sony")
 
-        expect(andrewGarfield.name).toBe("Spiderman Sony")
-        expect(andrewGarfield.age).toBe(31)
-        expect(andrewGarfield.actor).toBe("Andrew Garfield")
-        expect(andrewGarfield.movies).toBe(2)
-        expect(andrewGarfield.studio).toBe("Sony")
+        expect(andrewGarfield._name).toBe("Spiderman Sony")
+        expect(andrewGarfield._age).toBe(31)
+        expect(andrewGarfield._actor).toBe("Andrew Garfield")
+        expect(andrewGarfield._movies).toBe(2)
+        expect(andrewGarfield._studio).toBe("Sony")
 
 
     });
+
+    test('metodo getInfo from Spiderman Class', () => {
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31 , "Andrew Garfield", 2 , "Sony")
+
+        expect(andrewGarfield.getInfo()).toBe("Hey! im Andrew Garfield from Sony")
+
+    });
+
+    
   })
