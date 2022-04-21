5b05ce5ac00c42203ccc8f49425bbd8236bdb22e
Carlos Gerardo <carlosgerardo.rocha.s@gmail.com>
2022-04-21 07:18:16 -0500

    requirement 2: datecreated and lastUpdate instance from a date object define in the class object

--- weekly_mission_3/twitter/app/models/users.js
+++ weekly_mission_3/twitter/app/models/users.js
@@ -1,5 +1,12 @@
 class User{
-    
+    constructor(id, username, name, bio, dateCreated, lastUpdated){
+        this._id =id
+        this._username = username
+        this._name = name
+        this._bio = bio
+        this._dateCreated = new Date()
+        this._lastUpdate = new Date()
+    }
 }
 
 module.exports = User
--- weekly_mission_3/twitter/test/users.test.js
+++ weekly_mission_3/twitter/test/users.test.js
@@ -3,8 +3,14 @@ const User = require('./../app/models/users')
 describe("Pruebas de nuevo usuario", () => {
     test('Prueba de clase Usuario', () => {
 
-        const user1 = new User (1,'Carlos27','Carlos Rocha','Aqui va la bio', 'dateCreated', 'lastUpdate')
+        const user1 = new User (1,'Carlos27','Carlos Rocha','Aqui va la bio')
 
-        expect(user1.name).toBe(1);
-      });
+        expect(user1._id).toBe(1);
+        expect(user1._username).toBe('Carlos27');
+        expect(user1._name).toBe('Carlos Rocha');
+        expect(user1._bio).toBe('Aqui va la bio');
+        expect(user1._dateCreated).not.toBeUndefined();
+        expect(user1._lastUpdate).not.toBeUndefined();
+      
+    });
   });
