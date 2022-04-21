b42282273837658ade87ffe744acc91292fb0a2a
Carlos Gerardo <carlosgerardo.rocha.s@gmail.com>
2022-04-21 07:30:49 -0500

    Requirement 3: get methods were added

--- weekly_mission_3/twitter/app/models/users.js
+++ weekly_mission_3/twitter/app/models/users.js
@@ -1,11 +1,23 @@
 class User{
-    constructor(id, username, name, bio, dateCreated, lastUpdated){
+    constructor(id, username, name, bio){
         this._id =id
         this._username = username
         this._name = name
         this._bio = bio
         this._dateCreated = new Date()
-        this._lastUpdate = new Date()
+        this._lastUpdated = new Date()
+    }
+    get getUserName(){
+        return this._username
+    }
+    get getBio(){
+        return this._bio
+    }
+    get getDateCreated(){
+        return this._dateCreated
+    }
+    get getLastUpdated(){
+        return this._lastUpdated
     }
 }
 
--- weekly_mission_3/twitter/test/users.test.js
+++ weekly_mission_3/twitter/test/users.test.js
@@ -10,7 +10,19 @@ describe("Pruebas de nuevo usuario", () => {
         expect(user1._name).toBe('Carlos Rocha');
         expect(user1._bio).toBe('Aqui va la bio');
         expect(user1._dateCreated).not.toBeUndefined();
-        expect(user1._lastUpdate).not.toBeUndefined();
+        expect(user1._lastUpdated).not.toBeUndefined();
       
     });
+
+    test('prueba metodos get', () => {
+
+        const user1 = new User (1,'Carlos27','Carlos Rocha','Aqui va la bio')
+
+        expect(user1.getUserName).toBe('Carlos27');
+        expect(user1.getBio).toBe('Aqui va la bio');
+        expect(user1.getDateCreated).not.toBeUndefined();
+        expect(user1.getLastUpdated).not.toBeUndefined();
+      
+    })
+
   });
